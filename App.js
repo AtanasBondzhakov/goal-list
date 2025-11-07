f5faf4fdba448667cca411e04dcf161255329306
atanasbondzhakov <>
2025-11-07 15:35:12 +0200

    Add simpe containers

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Initial Screen</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.appContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.inputText} placeholder='Your new goal!' />
+                <Button title='Add Goal' color='#fff'/>
+            </View>
+            <View style={styles.goalListContainer}>
+                <Text>Goal List!!!</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+        flex: 1,
+        backgroundColor: '#cccccc'
+    },
+    inputContainer: {
+        flex: 1,
+        backgroundColor: '#293a9cff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop: 40,
+        paddingHorizontal: 32
+    },
+    goalListContainer: {
+        flex: 6,
+        backgroundColor: '#999898ff'
+    },
+    inputText: {
+        // borderColor: '#222',
+        borderRadius: 6,
+        borderWidth: 1,
+        width: '70%',
+        backgroundColor: '#ccc',
+        padding: 8
+    }
 });
