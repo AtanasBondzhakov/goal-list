dbeffc7f31f216b2fff2a8a537a248cefa2e887e
atanasbondzhakov <>
2025-11-12 15:09:59 +0200

    Add Modal wrapper for add new goal

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
-import GoalList from './components/goalList.jsx';
+import { Button, Modal, StyleSheet, TextInput, View } from 'react-native';
+import GoalList from './components/GoalList.jsx';
 
 export default function App() {
     const [enteredText, setEnteredText] = useState('');
     const [goalList, setGoalList] = useState([]);
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
     const inputTextHandler = (enteredText) => {
         setEnteredText(enteredText);
@@ -16,26 +17,48 @@ export default function App() {
             return [...state, enteredText];
         });
         setEnteredText('');
+        setIsModalVisible(false);
+    }
+
+    const toggleModalVisibility = () => {
+        setIsModalVisible(state => !state);
+        setEnteredText('');
     }
 
     return (
         <>
             <StatusBar style='light' />
             <View style={styles.appContainer}>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.inputText}
-                        placeholder='Your new goal!'
-                        onChangeText={inputTextHandler}
-                        value={enteredText}
-                    />
+                <View style={styles.initialScreenContainer}>
                     <Button
-                        title='Add Goal'
+                        title='Add New Goal'
                         color='#fff'
-                        onPress={addGoalHandler}
+                        onPress={toggleModalVisibility}
                     />
                 </View>
-                <GoalList goalList={goalList}/>
+                <Modal visible={isModalVisible}>
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={styles.inputText}
+                            placeholder='Your new goal!'
+                            onChangeText={inputTextHandler}
+                            value={enteredText}
+                        />
+                        <View style={styles.button}>
+                            <Button
+                                title='Cancel'
+                                color='#fff'
+                                onPress={toggleModalVisibility}
+                            />
+                            <Button
+                                title='Add Goal'
+                                color='#fff'
+                                onPress={addGoalHandler}
+                            />
+                        </View>
+                    </View>
+                </Modal>
+                <GoalList goalList={goalList} />
             </View>
         </>
     );
@@ -49,34 +72,27 @@ const styles = StyleSheet.create({
     inputContainer: {
         flex: 1,
         backgroundColor: '#293a9cff',
-        flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         paddingTop: 40,
-        paddingHorizontal: 32
+        paddingHorizontal: 32,
+        justifyContent: 'center'
     },
-    // goalListContainer: {
-    //     flex: 6,
-    //     backgroundColor: '#999898ff',
-    //     paddingTop: 16
-    // },
+    button: {
+        flexDirection: 'row',
+        width: 200,
+        paddingTop: 16,
+        justifyContent: 'space-between'
+    },
     inputText: {
-        // borderColor: '#222',
         borderRadius: 6,
         borderWidth: 1,
         width: '70%',
         backgroundColor: '#ccc',
         padding: 8
     },
-    // goalItem: {
-    //     backgroundColor: '#4d6cc2ff',
-    //     borderWidth: 1,
-    //     padding: 16,
-    //     margin: 6,
-    //     borderRadius: 6
-    // }, 
-    // goalText: {
-    //     color: '#dadae0ff',
-    //     fontSize: 16
-    // }
+    initialScreenContainer: {
+        paddingTop: 150,
+        backgroundColor: '#503535ff'
+    }
 });
